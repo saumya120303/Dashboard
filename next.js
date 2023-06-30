c580980d6a18d53f0a5032865353de1f0089a3ef
Saumya <saumya12032003@gmail.com>
2023-06-30 14:14:07 +0530

    Dashboard

---
+++ next.js
@@ -0,0 +1,8 @@
+// Create container element
+const container = document.createElement('div');
+container.id = 'container';
+document.body.appendChild(container);
+
+function redirectToDashboard() {
+    window.location.href = "dashboard.html";
+  }
